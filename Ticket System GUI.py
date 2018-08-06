c419b3b2b6e5cdc4be2fc53c54edead07df2dfef
Bryson Chen <cde123456780@Brysons-MacBook-Air.local>
2018-08-07 11:36:36 +1200

    New Update
    - Error Checking
    - New Adding Ticket Functionality
    - Additional Features

--- Ticket System GUI.py
+++ Ticket System GUI.py
@@ -33,6 +33,10 @@ numberTickets = IntVar()
 soldTickets = 0
 earnings = 0
 
+#For adding new tickets
+newName = StringVar()
+newPrice = DoubleVar()
+newCapacity = IntVar()
 
 def create1():
     #Display Prices
@@ -42,7 +46,10 @@ def create1():
     Label(frame1, text = "CURRENT SHOWINGS").grid(row=0,column=0)
     rows = 1
     for i in ticket:
-        Label(frame1,text = "{} SHOW - {} available/{} capacity (${} each)".format(i._time, i._availability, i._capacity, i._cost)).grid(row=rows, column = 0) 
+        if i._availability == 0:
+            Label(frame1,text = "{} SHOW - {} available/{} capacity (${:.2f} each)".format(i._time, i._availability, i._capacity, i._cost), fg = "red").grid(row=rows, column = 0)
+        else:
+            Label(frame1,text = "{} SHOW - {} available/{} capacity (${:.2f} each)".format(i._time, i._availability, i._capacity, i._cost)).grid(row=rows, column = 0) 
         rows +=1
         
     return frame1
@@ -76,9 +83,12 @@ def sell():
         for i in ticket:
             if i._time == ticketType.get():
                 if (i._availability - numberTickets.get()) >= 0:
-                    i._availability -= numberTickets.get()
-                    soldTickets += numberTickets.get()
-                    earnings += (i._cost * numberTickets.get())
+                    if numberTickets.get() <= 0:
+                        messagebox.showinfo("Warning", "Please enter an intger greater than 0")
+                    else:
+                        i._availability -= numberTickets.get()
+                        soldTickets += numberTickets.get()
+                        earnings += (i._cost * numberTickets.get())
                 else:
                     messagebox.showinfo("Warning", "Not enough Tickets")
 
@@ -96,7 +106,7 @@ def sell():
 
 def create3():
     frame3 = Frame(root, relief = "groove", borderwidth = 2, width = 50)
-    frame3.grid(row=2, column=0, sticky = N+E+W+S)
+    frame3.grid(row=1, column=0, sticky = N+E+W+S)
 
     Label(frame3, text = "SUMMARY").grid(row=0,column=0)
     Label(frame3, text = "{} tickets sold today".format(soldTickets)).grid(row=2,column=0)
@@ -106,7 +116,7 @@ def create3():
 
 def create4():
     frame4 = Frame(root, relief = "groove", borderwidth = 2, width = 50)
-    frame4.grid(row=2,column=1,sticky=N+W+E+S)
+    frame4.grid(row=1,column=1,sticky=N+W+E+S)
 
     Button(frame4, text = "RESET SHOWS", command = reset).grid(row=0,column=0)
     return frame4
@@ -121,10 +131,17 @@ def reset():
     global ticketType
     global numberTickets
 
+    global newName
+    global newCapacity
+    global newPrice
+
     earnings = 0
     soldTickets = 0
     ticketType.set("TIME")
     numberTickets.set(0)
+    newName.set("")
+    newCapacity.set(0)
+    newPrice.set(0)
 
     for i in ticket:
         i._restore()
@@ -137,6 +154,49 @@ def reset():
 
     frame3.destroy()
     frame3 = create3()
+
+def create5():
+    frame5 = Frame(root, relief = "groove", borderwidth =2, width = 50)
+    frame5.grid(row=2, column=0, sticky = N+S+W+E)
+
+    Label(frame5, text = "ADD NEW TICKET TYPE").grid(column=0,row=0)
+    Label(frame5, text = "NAME: ").grid(row=1,column=0)
+    Label(frame5, text = "CAPACITY: ").grid(row=2,column=0)
+    Label(frame5, text = "PRICE: ").grid(row=3,column=0)
+    Entry(frame5, textvariable = newName).grid(row=1,column=1)
+    Entry(frame5, textvariable = newCapacity).grid(row=2,column=1)
+    Entry(frame5, textvariable = newPrice).grid(row=3,column=1)
+    Button(frame5, text = "ADD TICKETS", command = addTicket).grid(row=4,column=0)
+    return frame5
+
+def addTicket():
+    global newName
+    global newCapacity
+    global newPrice
+
+    global frame1
+    global frame2
+    global frame3
+
+    duplicate = "NO"
+    for i in ticket:
+        if i._time == newName.get().upper():
+            duplicate = "YES"
+            break
+    if duplicate == "NO":
+        Ticket(newName.get().upper(), newCapacity.get(), newPrice.get())
+        
+        frame1.destroy()
+        frame1 = create1()
+
+        frame2.destroy()
+        frame2 = create2()
+
+        frame3.destroy()
+        frame3 = create3()
+    else:
+        messagebox.showinfo("Warning", "Ticket already exists")
+
     
 
 
@@ -144,5 +204,7 @@ frame1 = create1()
 frame2 = create2()
 frame3 = create3()
 frame4 = create4()
+frame5 = create5()
 
+root.mainloop()
         
